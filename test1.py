78f4dd8ce94cbf625c60d0183d1040e2e4b8319b
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-30 22:56:15 +0530

    no

--- test1.py
+++ test1.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 30 22:07:07 2019
+Created on Sat Mar 30 22:13:41 2019
 
 @author: abhijithneilabraham
 """
 
-import numpy
-import matplotlib.pyplot as plt
-import pandas
-import math
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-numpy.random.seed(7)
-dataframe = pandas.read_csv('final.csv', usecols=[1], engine='python', skipfooter=3)
-dataset = dataframe.values
-dataset = dataset.astype('float32')
-# normalize the dataset
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print(len(train), len(test))
+import csv
+import random
+lines=[]
+lines2=[]
+for i in range(0,10):
+    lines.append(random.randint(920,970))
+print(lines)
+line=[lines,[0]]
+csv.register_dialect("comma", delimiter=",")
+
+with open('people.csv', 'w+') as writeFile:
+    writer = csv.writer(writeFile,dialect="comma")
+  
+    writer.writerows(line)
+   
+
+
---
+++ test2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 22:18:10 2019
+
+@author: abhijithneilabraham
+"""
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load the dataset
+dataframe = pandas.read_csv('final.csv', usecols=[0], engine='python', skipfooter=3)
+dataset = dataframe.values
+print(len(dataset))
