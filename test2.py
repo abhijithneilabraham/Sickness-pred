89f16d53765ca6040a26e7661dab9a110b0d43b8
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-31 00:09:18 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,7 +5,7 @@ Created on Sat Mar 30 22:18:10 2019
 
 @author: abhijithneilabraham
 """
-import numpy
+from numpy import array
 import matplotlib.pyplot as plt
 import pandas
 import math
@@ -20,13 +20,31 @@ numpy.random.seed(7)
 # load the dataset
 dataset = numpy.loadtxt("dataset.csv", delimiter=",")
 
-
+def split_sequence(sequence, n_steps):
+	X, y = list(), list()
+	for i in range(len(sequence)):
+		# find the end of this pattern
+		end_ix = i + n_steps
+		# check if we are beyond the sequence
+		if end_ix > len(sequence)-1:
+			break
+		# gather input and output parts of the pattern
+		seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
+		X.append(seq_x)
+		y.append(seq_y)
+	return array(X), array(y)
 X = dataset[:,0]
 Y = dataset[:,1]
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=1, activation='relu'))
-model.add(Dense(8, activation='relu'))
+#model.add(Dense(12, input_dim=1, activation='relu'))
+n_steps=10
+n_features=1
+print(X)
+
+X = X.reshape((X.shape[0], X.shape[1], n_features))
+
+model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
 model.add(Dense(1, activation='sigmoid'))
 # Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
