49b27a4991f60d18ad2e92b142a3f62bb1d26f17
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-30 22:12:19 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -22,3 +22,8 @@ dataset = dataset.astype('float32')
 # normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
