5a924966a7e717b5c58f77d11199c7ac55f53102
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-30 23:49:09 +0530

    done uploading

--- test2.py
+++ test2.py
@@ -23,6 +23,19 @@ dataset = numpy.loadtxt("dataset.csv", delimiter=",")
 
 X = dataset[:,0]
 Y = dataset[:,1]
-
-
-print(Y)
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=1, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X, Y, epochs=150, batch_size=10)
+# evaluate the model
+scores = model.evaluate(X, Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+predictions = model.predict(X)
+# round predictions
+rounded = [round(x[0]) for x in predictions]
+print(rounded)
