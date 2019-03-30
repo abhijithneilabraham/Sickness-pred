447cecbf25e0a0e011bd8d10b977188e3f655582
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-30 23:17:03 +0530

    up

--- test1.py
+++ test1.py
@@ -13,12 +13,26 @@ import random
 
 with open('people.csv', 'w+') as writeFile:
     writer = csv.writer(writeFile)
-    for i in range(0,1372):
+    for i in range(0,1376):
         
         line=[[random.randint(920,970)]]
         
   
         writer.writerows(line)
         
+with open('people.csv','r') as csvinput:
+    with open('output.csv', 'w') as csvoutput:
+        writer = csv.writer(csvoutput, lineterminator='\n')
+        reader = csv.reader(csvinput)
 
+        all = []
+        row = next(reader)
+        row.append('0')
+        all.append(row)
+
+        for row in reader:
+            row.append(row[0])
+            all.append(row)
+
+        writer.writerows(all)
 
