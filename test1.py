6b660620bae37e0975044af415ef18e84b3c248c
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-03-30 22:12:04 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -15,3 +15,10 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+numpy.random.seed(7)
+dataframe = pandas.read_csv('final.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
