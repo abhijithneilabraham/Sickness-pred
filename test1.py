fbb6aabe344e4274c2bf0814af8cac40f599e962
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2019-04-24 00:21:17 +0530

    uploaded the dataset

--- test1.py
+++ test1.py
@@ -10,21 +10,23 @@ import csv
 import random
 x=[]
 
-
+'''
 with open('people.csv', 'w+') as writeFile:
     writer = csv.writer(writeFile)
     for i in range(0,1376):
         
         line=[[random.randint(920,970)]]
+
         
   
-        writer.writerows(line)        
-with open('people.csv','r') as csvinput:
+        writer.writerows(line)   
+'''
+with open('final_dataset1.csv','r') as csvinput:
     with open('output.csv', 'w') as csvoutput:
         writer = csv.writer(csvoutput, lineterminator='\n')
         reader = csv.reader(csvinput)
 
-        for j in range(1372):
+        for j in range(10248):
             
             all = []
             row = next(reader)
@@ -37,12 +39,12 @@ with open('people.csv','r') as csvinput:
         
 
             writer.writerows(all)
-with open('final.csv','r') as csvinput:
+with open('final_dataset2.csv','r') as csvinput:
     with open('output2.csv', 'w') as csvoutput:
         writer = csv.writer(csvoutput, lineterminator='\n')
         reader = csv.reader(csvinput)
 
-        for j in range(1372):
+        for j in range(1894):
             
             all = []
             row = next(reader)
